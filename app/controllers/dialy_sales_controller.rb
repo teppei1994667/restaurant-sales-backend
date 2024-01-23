3f145fbdc4cb9e4dd7be57efdce7f0d49c5b2654
teppei1994667 <teppei.engineer.667@gmail.com>
2024-01-23 22:58:13 +0900

    dialy_sales update 実装

--- app/controllers/dialy_sales_controller.rb
+++ app/controllers/dialy_sales_controller.rb
@@ -21,6 +21,17 @@ class DialySalesController < ApplicationController
 		end
 	end
 
+	# 売り上げを更新します。
+	def update
+		@dialy_sale = DialySale.find(params[:id])
+
+		if @dialy_sale.update(dialy_sale_params)
+			render json: @dialy_sale
+		else
+			render json: @dialy_sale.errors, status: :unprocessable_entity 
+		end
+	end
+
 	# 売り上げを削除します。
 	def destroy
 		dialy_sale_ids = params[:id].split(",")
