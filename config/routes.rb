77f1e408420bc50d9d0961264f24af1699de2f39
teppei1994667 <teppei.engineer.667@gmail.com>
2024-02-25 22:46:30 +0900

    registration ルーティング 設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registration: "auth/registration"
+  }
   resources :dialy_sales
 end
