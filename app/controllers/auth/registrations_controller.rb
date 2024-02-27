f7ddbfc53067f1eea4eb4caf01cf57d53ae64c20
teppei1994667 <teppei.engineer.667@gmail.com>
2024-02-27 23:01:56 +0900

    参考サイトの変更によりset_token_infoを一旦コメントアウト

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
 
 	# DeviseTokenAuth::RegistrationsControllerで定義されたcreateメソッド実行後にset_token_infoメソッドを実行
-	after_action :set_token_info, only: [:create]
+	# after_action :set_token_info, only: [:create]
 
 	private
 
@@ -11,12 +11,12 @@ class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
 
 	# ユーザーデータが保存されているか確認し、保存されていれば新しいトークンを作成。
   # ヘッダーに access-token と client を設定
-	def set_token_info
-		return unless @resource.persisted?
+	# def set_token_info
+	# 	return unless @resource.persisted?
 
-		token = @resource.create_new_auth_token
-		response.set_header("access-token", token["access-toke"])
-		response.set_header("client", token["client"])
-	end
+	# 	token = @resource.create_new_auth_token
+	# 	response.set_header("access-token", token["access-toke"])
+	# 	response.set_header("client", token["client"])
+	# end
 
 end
