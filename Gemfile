dfa8f20416a4def3ff322d436e093d52785a0d89
teppei1994667 <teppei.engineer.667@gmail.com>
2024-02-28 21:56:07 +0900

    コードの乱れが気になったのでrubocop導入

--- Gemfile
+++ Gemfile
@@ -46,7 +46,9 @@ group :development, :test do
 end
 
 group :development do
-  # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
+  gem 'rubocop', require: false # 追加
+  gem 'rubocop-performance', require: false # 追加
+  gem 'rubocop-rails', require: false # 追加
+  gem 'rubocop-rspec' # 追加
 end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
-class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  #用途不明のため調査
+  skip_before_action :verify_authenticity_token
+  
+  helper_method :current_user, :user_signed_in?
 end
