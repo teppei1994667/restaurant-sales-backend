e11adb12fdc7d8122f4268a1c4fb4a390a59e553
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-02 12:22:53 +0900

    sessionsのルーティング 追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registration: "auth/registration"
   }
+
+  namespace :auth do
+    resource :sessions, only: %i[index]
+  end
   resources :dialy_sales
 end
