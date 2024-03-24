f5f5f58d36b7f2be28826dffdf6531a31191e8d6
teppei1994667 <teppei.engineer.667@gmail.com>
2024-03-24 13:23:37 +0900

    ユーザーのログアウト機能実装

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -6,4 +6,21 @@ class Auth::SessionsController < DeviseTokenAuth::SessionsController
       render json: { is_login: false, message: 'ユーザーが存在しません' }
     end
   end
+
+  def destroy
+    # 認証情報を含むヘッダーからトークン情報を取得
+    client_id = request.headers["client"]
+    uid = request.headers["uid"]
+    access_token = request.headers["access-token"]
+
+    # トークン情報を使用してユーザーを特定し、トークンを無効化する
+    user = User.find_by_uid(uid)
+    user.tokens.delete(client_id) if user
+
+    if user&.save
+      render json: { message: 'ログアウトしました。' }
+    else
+      render json: { errors: ['ログアウトに失敗しました。'] }, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    sessions: 'auth/sessions'
+  }
+
   resources :users
   resources :dialy_sales
 end
