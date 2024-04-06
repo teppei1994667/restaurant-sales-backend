009eaab0c10e76c6c3f3e7b50224468dd4bfb2a6
teppei1994667 <teppei.engineer.667@gmail.com>
2024-04-06 18:36:22 +0900

    リクエストされたユーザー情報を返却 実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render json: { message: 'hello' }
-  end
-
-  def show
-    @user = User.find(params[:id])
+		@user = User.find_by(email: request.headers["uid"])
     render json: @user
   end
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
 end
