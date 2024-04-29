10fd2885893593c64b5cac40909f9eb678f8d162
teppei1994667 <teppei.engineer.667@gmail.com>
2024-04-29 21:41:37 +0900

    不要なコメント削除

--- app/controllers/dialy_sales_controller.rb
+++ app/controllers/dialy_sales_controller.rb
@@ -2,12 +2,12 @@ class DialySalesController < ApplicationController
   # 売上一覧をJson形式で返却します
   def index
     @dialy_sale = DialySale.new
-    if params[:start_day] && params[:end_day] # rubocop:disable Style/ConditionalAssignment
+    if params[:start_day] && params[:end_day]
       dialy_sales = DialySale.where(sales_day: params[:start_day]..params[:end_day])
     else
       dialy_sales = DialySale.all
     end
-		@return_dialy_sales = @dialy_sale.convert_dialy_sales(dialy_sales) # rubocop:disable Layout/IndentationStyle,Layout/IndentationConsistency
+		@return_dialy_sales = @dialy_sale.convert_dialy_sales(dialy_sales)
     render json: @return_dialy_sales
   end
 
