633e7fda89595682f3ba4d6152145fe56f32905c
teppei1994667 <teppei.engineer.667@gmail.com>
2024-04-29 22:59:47 +0900

    UserController 紐づくstoreを返却

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = User.find_by(email: request.headers["uid"])
-    render json: @user
+    user = User.find_by(email: request.headers["uid"])
+    store = Store.where(user_id: user.id)
+    render json: {"user" => user, "store" => store}
   end
 end
