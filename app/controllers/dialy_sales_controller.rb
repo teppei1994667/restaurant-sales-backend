78c9085e1324c14e9eadd054323c5482c0da4349
teppei1994667 <teppei.engineer.667@gmail.com>
2024-05-27 23:50:38 +0900

    DialySale取得のデフォルトの期間指定をサーバー側で行う

--- app/controllers/dialy_sales_controller.rb
+++ app/controllers/dialy_sales_controller.rb
@@ -1,26 +1,29 @@
 class DialySalesController < ApplicationController
+
+  # クライアントから期間が指定が無い場合のデフォルト値(当月月初から当日)
+  # グローバル変数として定義しているのでスコープが存在しないデメリットがあるので再度検討
+  $start_day = Date.today.at_beginning_of_month.strftime("%Y-%m-%d")
+  $today = Time.zone.today.strftime("%Y-%m-%d")
+
   # 売上一覧をJson形式で返却します
   def index
-    @dialy_sales = DialySale.new
+    dialy_sale = DialySale.new
     if params[:start_day] && params[:end_day]
       dialy_sales = DialySale.where(store_id: params[:store_id], sales_day: params[:start_day]..params[:end_day])
     else
-      dialy_sales = DialySale.all
+      dialy_sales = DialySale.where(store_id: params[:store_id], sales_day: $start_day..$today)
     end
-		@return_dialy_sales = @dialy_sales.convert_dialy_sales(dialy_sales)
-    render json: @return_dialy_sales
+		return_dialy_sales = dialy_sale.convert_dialy_sales(dialy_sales)
+    render json: return_dialy_sales
   end
 
   # 新規の売上を作成します。
   def create
     @new_dialy_sale = DialySale.new(dialy_sale_params)
+    p $start_day
     if @new_dialy_sale.save
-      @dialy_sales = DialySale.new
-      if params[:start_day] && params[:end_day]
-        dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id], sales_day: params[:start_day]..params[:end_day])
-      else
-        dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id])
-      end
+      dialy_sales = DialySale.new
+      dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id], sales_day: $start_day..$today)
       @return_dialy_sales = @dialy_sales.convert_dialy_sales(dialy_sales)
       render json: @return_dialy_sales
     else
@@ -31,14 +34,9 @@ class DialySalesController < ApplicationController
   # 売り上げを更新します。
   def update
     @dialy_sale = DialySale.find(params[:id])
-
     if @dialy_sale.update(dialy_sale_params)
       @dialy_sales = DialySale.new
-      if params[:start_day] && params[:end_day]
-        dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id], sales_day: params[:start_day]..params[:end_day])
-      else
-        dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id])
-      end
+      dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id], sales_day: $start_day..$today)
       @return_dialy_sales = @dialy_sales.convert_dialy_sales(dialy_sales)
       render json: @return_dialy_sales
       # render json: @dialy_sale
@@ -54,11 +52,7 @@ class DialySalesController < ApplicationController
       DialySale.find(dialy_sale_id).destroy
     end
     @dialy_sales = DialySale.new
-    if params[:start_day] && params[:end_day]
-      dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id], sales_day: params[:start_day]..params[:end_day])
-    else
-      dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id])
-    end
+    dialy_sales = DialySale.where(store_id: dialy_sale_params[:store_id], sales_day: $start_day..$today)
     @return_dialy_sales = @dialy_sales.convert_dialy_sales(dialy_sales)
     render json: @return_dialy_sales
   end
